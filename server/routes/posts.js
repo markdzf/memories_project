c1573d09dc46ee636b01737843709d9125e3c1a1
markdzf <markdarwinzfababaer@gmail.com>
2022-02-09 21:55:52 +0800

    FE and BE setup

---
+++ server/routes/posts.js
@@ -0,0 +1,10 @@
+// const  express =  require('express');
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('This works');
+});
+
+export default router;
